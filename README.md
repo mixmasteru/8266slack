3cc45ae4889e15f3b49435bc6b075100a753120e
Ulrich Pech <ulrichpech@gmail.com>
2018-10-11 17:36:54 +0200

    readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 8266slack
 use an ESP8266 to connect to slack and display messages on a e paper display
+
+## copyrigths
+The code for connecting to slack is from [arduino-slack-bot](https://github.com/urish/arduino-slack-bot)
--- src/secrets.h
+++ src/secrets.h
@@ -1,4 +1,4 @@
 // thats a "legacy token" see https://api.slack.com/custom-integrations/legacy-tokens
-#define SLACK_BOT_TOKEN       "tocke"
+#define SLACK_BOT_TOKEN       "token"
 #define WIFI_SSID             "SSID"
 #define WIFI_PASSWORD         "PWD"
